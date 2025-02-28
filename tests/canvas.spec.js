96e94813247494a74a6a5416c260e01ae34d3d65
Ankit Keshari <ankit.keshari@nitorinfotech.com>
2025-02-28 15:00:55 +0530

    Worked on the canvas related test cases

--- tests/canvas.spec.js
+++ tests/canvas.spec.js
@@ -12,3 +12,43 @@ test("Render canvas", async ({ page }) => {
   // Check if the React Flow container is visible
   await expect(reactFlowContainer).toBeVisible()
 })
+
+test("Render Minimap", async ({ page }) => {
+  // Variable
+  const miniMap = page.locator(".react-flow__minimap")
+
+  // Check if the Minimap is visible
+  await expect(miniMap).toBeVisible()
+})
+
+test("Render Controls", async ({ page }) => {
+  // Variable
+  const controls = page.locator(".react-flow__controls")
+
+  // Check if the controls is visible
+  await expect(controls).toBeVisible()
+})
+
+test("Zoom In Canvas", async ({ page }) => {
+  const zoomInControl = page.locator(".react-flow__controls-zoomin")
+
+  await zoomInControl.hover()
+  await page.mouse.down()
+  await page.mouse.up()
+})
+
+test("Zoom Out Canvas", async ({ page }) => {
+  const zoomOutControl = page.locator(".react-flow__controls-zoomout")
+
+  await zoomOutControl.hover()
+  await page.mouse.down()
+  await page.mouse.up()
+})
+
+test("Fit View Canvas", async ({ page }) => {
+  const fitViewControl = page.locator(".react-flow__controls-fitview")
+
+  await fitViewControl.hover()
+  await page.mouse.down()
+  await page.mouse.up()
+})
